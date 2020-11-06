80a9398de456bea66d12df8a89e67c728727bd05
Natalia Cristobal <natalia-c-r@hotmail.com>
2020-11-06 17:42:52 +0100

    Cambio de linea en nuevaClase, carpetaInstituto

--- Practica3/src/NuevaClase.java
+++ Practica3/src/NuevaClase.java
@@ -1,5 +1,5 @@
 public class NuevaClase {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo, practica de entornos");
     }
 }
